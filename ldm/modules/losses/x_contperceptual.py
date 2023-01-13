a6a335b1e5da3c96f55151e3b4d3f1960e667f77
章乐 <lezhang.thu@gmail.com>
2023-01-13 15:42:18 +0800

    missing

--- ldm/modules/losses/x_contperceptual.py
+++ ldm/modules/losses/x_contperceptual.py
@@ -1,5 +1,20 @@
+import functools
+
 import torch
 import torch.nn as nn
+
+
+def hinge_d_loss(logits_real, logits_fake):
+    loss_real = torch.mean(F.relu(1. - logits_real))
+    loss_fake = torch.mean(F.relu(1. + logits_fake))
+    d_loss = 0.5 * (loss_real + loss_fake)
+    return d_loss
+
+
+def vanilla_d_loss(logits_real, logits_fake):
+    d_loss = 0.5 * (torch.mean(torch.nn.functional.softplus(-logits_real)) +
+                    torch.mean(torch.nn.functional.softplus(logits_fake)))
+    return d_loss
 
 
 def weights_init(m):
